14bec1eef6a8794bbffca6f424eb6afcac3c579f
FlorianU <florian.a.unger@gmail.com>
2023-03-07 17:35:59 +0100

    installed tailwindcss

--- personal-portfolio/src/components/desktop-introduction.tsx
+++ personal-portfolio/src/components/desktop-introduction.tsx
@@ -2,8 +2,8 @@ import "./desktop-introduction.css";
 
 const DesktopIntroduction = () => {
   return (
-    <div className="">
-      <p>INTROOOO</p>
+    <div className="bg-blue-100">
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
     </div>
   );
 };
