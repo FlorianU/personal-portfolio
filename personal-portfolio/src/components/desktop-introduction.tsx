8122825ed4c499e3e0298b237db67b28c080dc59
FlorianU <florian.a.unger@gmail.com>
2023-03-08 13:38:18 +0100

    implemented introduction parallax

--- personal-portfolio/src/components/desktop-introduction.tsx
+++ personal-portfolio/src/components/desktop-introduction.tsx
@@ -1,9 +1,23 @@
 import "./desktop-introduction.css";
+import endlessCorridor from "../assets/images/endless_corridor.svg";
 
 const DesktopIntroduction = () => {
   return (
-    <div className="bg-blue-100">
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+    <div className="page">
+      <div className="keyart" id="parallax">
+        <div className="keyart_layer" id="keyart-8"></div>
+        <div className="keyart_layer" id="keyart-7"></div>
+        <div className="keyart_layer" id="keyart-6"></div>
+        <div className="keyart_layer" id="keyart-5"></div>
+        <div className="keyart_layer" id="keyart-4"></div>
+        <div className="keyart_layer" id="keyart-3"></div>
+        <div className="keyart_layer" id="keyart-2"></div>
+        <div className="keyart_layer" id="keyart-1"></div>
+        <div className="keyart_layer" id="keyart-0"></div>
+      </div>
+      <div className="overflow">
+        <h1>CSS Parallax Scrolling</h1>
+      </div>
     </div>
   );
 };
